0e6ac001f1b83599353f87f9d4f075b5d532645e
Jon Sherrard <jon@andthats.it>
2015-09-28 12:06:46 +0100

    Add the Passport to the promos colu,n

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php namespace AgreablePromoPlugin\Controllers;
+
+use AgreablePromoPlugin\Helper;
+
+class AdminController {
+
+  public function modify_admin(){
+
+    /*
+     * @AgreablePromoPlugin is a Twig namespace which Herbert generates from
+     * values in herbert.config.php.
+     * @see http://twig.sensiolabs.org/doc/api.html#loaders
+     *
+     * Using get_field() which is an ACF function to retrieve theme
+     * specific options affecting the style of the promo.
+     * ACF definitions for Panel are in app/panels.php.
+     */
+
+    \Jigsaw::show_notice('Jigsaw is working', 'updated'); 
+    \Jigsaw::add_column('promo', 'Passport', function($pid){
+      // This is a render callback
+      $data = array();
+      $data = new \TimberPost($pid);
+      echo json_decode($data->selected_passport)->id;
+    }, 5);
+
+  }
+
+}
--- app/Hooks/SLMPluginEnqueue.php
+++ app/Hooks/SLMPluginEnqueue.php
@@ -2,17 +2,25 @@
 
 use AgreablePromoPlugin\Helper;
 use AgreablePromoPlugin\Controllers\RenderController;
+use AgreablePromoPlugin\Controllers\AdminController;
 use Herbert\Framework\Enqueue;
 
 class SLMPluginEnqueue {
 
   public function init() {
     \add_action('agreable_promo_plugin_enqueue', array($this, 'plugin_enqueue'), 10, 0);
+    \add_action('admin_init', array($this, 'admin_enqueue'), 10, 0);
   }
 
   public function plugin_enqueue(){
     $r = new RenderController();
     $r->enqueue();
+  }
+
+  public function admin_enqueue(){
+    $a = new AdminController();
+    $a->modify_admin();
+
   }
 
 }
