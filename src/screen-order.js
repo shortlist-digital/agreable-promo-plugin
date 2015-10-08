5a257247390003776031209655f999e801883c96
Jon Sherrard <jon@andthats.it>
2015-10-08 16:33:20 +0100

    starting screen order definitions

---
+++ src/screen-order.js
@@ -0,0 +1,13 @@
+import { ScreenNames } from './actions'
+export const standardOrder = [
+	ENTER_SCREEN,
+	FORM_SCREEN,
+	THANK_YOU_SCREEN
+]
+
+export const competitionOrder = [
+	ENTER_SCREEN,
+	COMPETITION_SCREEN,
+	FORM_SCREEN,
+	THANK_YOU_SCREEN
+]
