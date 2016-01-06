5279be4bc71374a48dec66a02145cf5e75a34f4b
garethfoote <gareth.foote@gmail.com>
2016-01-06 16:19:58 +0000

    Removing legacy conditionals.

--- app/hooks.php
+++ app/hooks.php
@@ -6,14 +6,6 @@ use AgreablePromoPlugin\Hooks\TimberTwig;
 use AgreablePromoPlugin\Hooks\SLMPluginEnqueue;
 use AgreablePromoPlugin\Hooks\SavePost;
 
-if(class_exists('AgreablePromoPlugin\Hooks\TimberTwig')){
-  (new TimberTwig)->init();
-}
-
-if(class_exists('AgreablePromoPlugin\Hooks\SLMPluginEnqueue')){
-  (new SLMPluginEnqueue)->init();
-}
-
-if(class_exists('AgreablePromoPlugin\Hooks\SavePost')){
-  (new SavePost)->init();
-}
+(new TimberTwig)->init();
+(new SLMPluginEnqueue)->init();
+(new SavePost)->init();
